5e40f3e648b74aa6eee46ef3818401ebe6b0153f
User <koreshsashano@gmail.com>
2022-10-29 14:00:07 +0300

    З ЧИМ буде жоская работа

--- main.py
+++ main.py
@@ -7,4 +7,19 @@ class Human:
         self.car = car
         self.glabhess = 50
         self.stiety = 50
-
+    def get_home(self):
+        pass
+    def get_car(self):
+        pass
+    def get_job(self):
+        pass
+    def eat(self):
+        pass
+    def work(self):
+        pass
+    def shopping(self):
+        pass
+    def chill(self):
+        pass
+    def clear_home(self):
+        pass
