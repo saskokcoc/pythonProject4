faa8dfda18ebfc8938e112cfe999ba9dd54221e3
User <koreshsashano@gmail.com>
2022-10-29 13:16:23 +0300

    + опис сім

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+class Human:
+    def __init__(self, name="Альбердо", job = None, home = None, car = None):
+        self.name = name
+        self.job = job
+        self.home = home
+        self.car = car
+        self.glabhess = 50
+        self.stiety = 50
